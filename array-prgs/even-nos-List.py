f2b76b5205100ee6911e8ddd359df7120f785f53
vinay <vinayn987vn@gmail.com>
2023-12-21 14:07:04 +0530

    commit

---
+++ array-prgs/even-nos-List.py
@@ -0,0 +1,9 @@
+"""list = [1, 2, 3, 4, 5, 6, 7, 8]
+for i in list:
+    if i % 2 == 0:
+        print(i, end=" ")"""
+
+list1 = [1, 2, 23, 41, 55, 9, 7, 8]
+for i in list1:
+    if i % 2 != 0:
+        print(i, end=" ")
