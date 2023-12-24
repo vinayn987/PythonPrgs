98e314b3c5a7a73e70da7f480eff491b02c81730
vinay <vinayn987vn@gmail.com>
2023-12-24 21:30:43 +0530

    commit

--- addition-of-two-no.py
+++ addition-of-two-no.py
@@ -3,3 +3,13 @@ number2 = 10
 
 sum = number1 + number2
 print(f"The addition of {number1} and {number2} is {sum}")
+
+print(type(number1))
+print(isinstance(number1, int))
+
+def sum(x, y):
+    return x+y
+
+add = sum(number1, number2)
+print(type(add)) # Checking the data type
+print(isinstance(add, int)) # checking the type of variable
