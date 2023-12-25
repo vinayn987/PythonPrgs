bc8778085b65c92712d93c0861a6974908e95200
vinay <vinayn987vn@gmail.com>
2023-12-25 19:01:45 +0530

    commit

--- string-prgs/even-str.py
+++ string-prgs/even-str.py
@@ -1,18 +1,18 @@
 # python program to print even words 
-string = "we are violent men" # In this example 'we' word has 2 letters and 
+string = "we are all violent men " # In this example 'we' word has 2 letters and 
                               # ramain words are have odd no. of letters 
 
-k = 3
+
 
 count = string.split(' ')
 
 for i in count:
     if len(i)%2==0:
-        print(i, end=" ")
+        print(i, end="")
+# to print words greater than k
 
+k = 3
 for i in count:
     if len(i)>k:
-        print("\n",i, end=' ' )
+        print("\n", i)
 
-
-        
