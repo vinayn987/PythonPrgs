84379524beb3c629596421612fa01206dc939c23
vinay <vinayn987vn@gmail.com>
2024-01-02 00:04:42 +0530

    commit

--- PRACTICAL.py/BikeShopSystem.py
+++ PRACTICAL.py/BikeShopSystem.py
@@ -1,9 +1,11 @@
+# Creating a bike shop system using oop's concepts
 class BikeShop:
-   
+    #Creating a class variable
     stock = 100
     def available_bikes(self):
-        return self.stock
+        return self.stock # accessing and returning a class variable by default(self)  parameter
     
+    # function for rent logic
     def bike_for_rent(self, bike):
         if bike > self.stock:
             print("Sorry we don't have that much stock ")
@@ -14,24 +16,27 @@ class BikeShop:
             print(f"Total bikes {(self.stock - bike)}")
             print("Thank you for coming Visit again")
 
-res = BikeShop()
+res = BikeShop() # creating an object 
 
+# Testing the class BikeShop
 print('''
-    A.Availble stock
+    A.Availble stock        
     B.Bikes for rent 
     C.exit
 
-''')
+''')        # Note: don't print the function which printing itself 
+            # because it shows None value 
+            # If you write a return function then call the function in print()
 while True:
     order = input("Please enter your choice A\B\C ==> ")
     match order:
         case "A":
-            print(res.available_bikes())
+            print(res.available_bikes()) # calling the function
 
         case "B":
             print("     Please enter number of bikes")
             bike = int(input("===> "))
-            res.bike_for_rent(bike)
+            res.bike_for_rent(bike) # calling the function
         case "C":
             print(exit())
         
