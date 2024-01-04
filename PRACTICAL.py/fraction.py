3b9e8d94c86e12ca13da1db87f951a679c80b82e
vinay <vinayn987vn@gmail.com>
2024-01-04 16:25:18 +0530

    commit

--- PRACTICAL.py/fraction.py
+++ PRACTICAL.py/fraction.py
@@ -1,16 +1,35 @@
+# creating a fraction datatype for myself using OOP
 class Fraction:
     
-    def __init__(self, n, d):
-        self.numer = n
-        self.denom = d
+    def __init__(self, __n, __d):
+        self.__numer = __n      # declaring the private variables
+        self.__denom = __d      
       
 
-    def __str__(self):
-        return f"{self.numer}/{self.denom}"
+    def __str__(self):          # returning the variables in fraction eg: 5/6
+        return f"{self.__numer}/{self.__denom}"
     
-    def __add__(self, other):
-        num = self.numer * other.denom + self.denom * other.numer
-        den = self.denom * other.denom
+    def __add__(self, other):   # this magic method add the two fraction values and return as fraction
+        __num = self.__numer * other.__denom + self.__denom * other.__numer
+        __den = self.__denom * other.__denom
+    
+        def simplify(n, d): # This method return simplified fraction 
+        
+            def find_gcf(a, b):
+                while b:
+                    a, b = b, a % b
+                return a
+            gcf = find_gcf(n, d)
+            n = __num // gcf
+            d = __den // gcf
+            return f"{n}/{d}"
+        n = simplify(__num, __den)
+        return n
+    
+    def __sub__(self, other): # It returns subtraction two fractions value
+
+        __num = self.__numer * other.__denom - self.__denom * other.__numer
+        __den = self.__denom * other.__denom
     
         def simplify(n, d):
         
@@ -19,16 +38,15 @@ class Fraction:
                     a, b = b, a % b
                 return a
             gcf = find_gcf(n, d)
-            n = num // gcf
-            d = den // gcf
+            n = __num // gcf
+            d = __den // gcf
             return f"{n}/{d}"
-        n = simplify(num, den)
+        n = simplify(__num, __den)
         return n
-    
-    def __sub__(self, other):
 
-        num = self.numer * other.denom - self.denom * other.numer
-        den = self.denom * other.denom
+    def __mul__(self, other): # It returns multiple of two fractions
+        __num = self.__numer * other.__denom * self.__denom * other.__numer
+        __den = self.__denom * other.__denom
     
         def simplify(n, d):
         
@@ -37,15 +55,15 @@ class Fraction:
                     a, b = b, a % b
                 return a
             gcf = find_gcf(n, d)
-            n = num // gcf
-            d = den // gcf
+            n = __num // gcf
+            d = __den // gcf
             return f"{n}/{d}"
-        n = simplify(num, den)
+        n = simplify(__num, __den)
         return n
-
-    def __mul__(self, other):
-        num = self.numer * other.denom * self.denom * other.numer
-        den = self.denom * other.denom
+    
+    def __truediv__(self, other): # It returns division of two fractions
+        __num = self.__numer * other.__denom / self.__denom * other.__numer
+        __den = self.__denom * other.__denom
     
         def simplify(n, d):
         
@@ -54,34 +72,18 @@ class Fraction:
                     a, b = b, a % b
                 return a
             gcf = find_gcf(n, d)
-            n = num // gcf
-            d = den // gcf
+            n = __num // gcf
+            d = __den // gcf
             return f"{n}/{d}"
-        n = simplify(num, den)
-        return n
-    
-    def __truediv__(self, other):
-        num = self.numer * other.denom / self.denom * other.numer
-        den = self.denom * other.denom
-    
-        def simplify(n, d):
-        
-            def find_gcf(a, b):
-                while b:
-                    a, b = b, a % b
-                return a
-            gcf = find_gcf(n, d)
-            n = num // gcf
-            d = den // gcf
-            return f"{n}/{d}"
-        n = simplify(num, den)
+        n = simplify(__num, __den)
         return n
 
         
-if __name__ == "__main__":    
+if __name__ == "__main__":    # This statemnt split the execution part and non-executable 
+                              # part Mostly it is useful in module concept  
 
-
-    obj = Fraction(5, 6)
-    obj1 = Fraction(17, 39)
-    add = obj + obj1
+    obj = Fraction(56, 6)
+    obj1 = Fraction(98, 39)
+    add = obj - obj1
     print(add)
+    
